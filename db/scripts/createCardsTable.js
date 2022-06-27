ad978316cf5d35ad469f184ed8d2ddab6310a136
ozzyee <oearle10@gmail.com>
2022-06-27 16:33:05 +0100

    create cards table script is set up

---
+++ db/scripts/createCardsTable.js
@@ -0,0 +1,20 @@
+import query from "../index.js";
+
+async function createCardsTable() {
+  const res = await query(`CREATE TABLE IF NOT EXISTS cards (
+    id SERIAL PRIMARY KEY,
+    usersID TEXT,
+    cardName TEXT,
+    cardNumber TEXT,
+    cardSortCode Text,
+    cardAccountNumber Text,
+    cardValidFrom TEXT,
+    cardExpiryEnd TEXT,
+    cardCvv TEXT,
+    cardPin TEXT,
+    cardInfo Text
+)`);
+  console.log("Created cards table: ", res);
+}
+
+createCardsTable();
